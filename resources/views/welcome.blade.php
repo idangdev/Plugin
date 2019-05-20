a2a6909d0a6a44aa7136cf21d94c19da168e8dda
pmlprogamer <pmlprogamer@gmail.com>
2019-05-20 21:31:38 +0700

    welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel <3 Heroku
                 </div>
 
                 <div class="links">
